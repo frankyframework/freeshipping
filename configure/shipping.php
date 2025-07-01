97a57f0410d773ffef3caeb6c73971dc7df8e47f
ussiel <ussiel@gmail.com>
2025-07-01 11:48:37 -0600

    ajustes

--- configure/shipping.php
+++ configure/shipping.php
@@ -1,11 +1,15 @@
 <?php
 return [
+    [
     "code" => "free_shipping",
     "data" => [
         "name" => getCoreConfig("ecommerce/free-shipping/titulo"),
         "enabled" => getEnabled(),
         "time" => getCoreConfig("ecommerce/free-shipping/dias"),
         "price" => 0,
+        "iva" => 0,
         "html" => "<p>* Tiempo expresado en dias habiles</p>"
+        ]
     ]
-    ];
+
+];
---
+++ loads/init.php
@@ -0,0 +1,2 @@
+<?php
+include 'util.php';
